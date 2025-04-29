48b8f1345b2633f0974c61387aa3dea7c17198f9
senpaiharde <xxslavan1@gmail.com>
2025-04-30 00:00:19 +0300

    feac LearningSessionSchema schema

---
+++ fitness-ai-tracker-backend/src/models/LearningSeason.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Types } from 'mongoose';
+
+
+export interface ILearningSession extends mongoose.Document{
+    userId: Types.ObjectId, 
+    date: Date,
+    topic: string,
+    startTime: string,
+    endTime: string,
+    notes? : string,
+}
+
+
+const LearningSessionSchema = new Schema<ILearningSession>(
+    {
+        userId:    { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+        date:      { type: Date, required: true, index: true },
+        topic:     { type: String, required: true },
+        startTime: String,
+        endTime:   String,
+        notes:     String
+    },
+    {timestamps: true}
+)
+
+LearningSessionSchema.index({userId: 1 , date: 1});
+export default mongoose.model<ILearningSession>('LearningSession',LearningSessionSchema);
