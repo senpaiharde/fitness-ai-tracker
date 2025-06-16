265e69d2b30931a2061bab980623e0bfe13352f2
senpaiharde <xxslavan1@gmail.com>
2025-06-16 19:31:38 +0300

    functions ready , i will move it to anotther folder next push

--- fitness-ai-tracker/src/components/AIForm/AiForm.tsx
+++ fitness-ai-tracker/src/components/AIForm/AiForm.tsx
@@ -11,6 +11,7 @@ import { selectToken, selectUser } from "../../features/user/userSlice";
 
 import DiaryPage from "./../FoodSearchModal/DiaryPage";
 import Learning from "./../Learning/learning";
+import { SvgArrow, SvgMicro, SvgPlus } from "./SvgBox.jsx";
 
 export const AiForm = () => {
     const token = useAppSelector(selectToken);
@@ -18,12 +19,33 @@ export const AiForm = () => {
 
     const isLoggedIn = Boolean(token);
     const [display, setdisplay] = useState("");
-    const [displaylog, setdisplaylog] = useState(false);
-    const [displayFood, setdisplayFood] = useState(false);
-    const [displayLearning, setdisplayLearning] = useState(false);
+    const [loading, setLoading] = useState(false);
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
+    const API_BASE =  "http://localhost:4000";
+    const handleChatAI = async () => {
+        if (!display.trim()) return alert("fill the text");
+        setLoading(true);
+        try {
+            const resp = await fetch(`${API_BASE}/ai/intake/`, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ prompt }),
+            });
+            const { data } = await resp.json();
+             if (!resp.ok) {
+        const errData = await resp.json();
+        throw new Error(errData.error || 'Failed To create Board!.');
+      }
+        } catch (err: any) {
+            console.error("err at creating board with ai:", err);
+            alert("failed to create board: " + err.message);
+        }
+    };
     return (
         <div className="AiChat">
             {isLoggedIn && (
@@ -35,24 +57,34 @@ export const AiForm = () => {
                             <input
                                 type="text"
                                 value={display}
+                                onChange={(e) => setdisplay(e.target.value)}
                                 placeholder="Ask anything"
                                 className="MainChatInput"
                             />
-                            <div className="MainChatContainerBotton">
-                                <button className="MainChatContainerButtonAdd"></button>
-                                <button 
-                                
-                                className="MainChatContainerButton">
-                                    <svg
-                                    
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        width={20}
-                                        height={20}
+                            <div className="MainChatContainerBottonArea">
+                                <div>
+                                    <button
+                                        title="Add photos and files"
+                                        className="MainChatContainerButtonAdd"
                                     >
-                                        <path d="M8.99992 16V6.41407L5.70696 9.70704C5.31643 10.0976 4.68342 10.0976 4.29289 9.70704C3.90237 9.31652 3.90237 8.6835 4.29289 8.29298L9.29289 3.29298L9.36907 3.22462C9.76184 2.90427 10.3408 2.92686 10.707 3.29298L15.707 8.29298L15.7753 8.36915C16.0957 8.76192 16.0731 9.34092 15.707 9.70704C15.3408 10.0732 14.7618 10.0958 14.3691 9.7754L14.2929 9.70704L10.9999 6.41407V16C10.9999 16.5523 10.5522 17 9.99992 17C9.44764 17 8.99992 16.5523 8.99992 16Z"></path>
-                                    </svg>
-                                </button>
+                                        <SvgPlus />
+                                    </button>
+                                </div>
+
+                                <div>
+                                    <button
+                                        title="Add photos and files"
+                                        className="MainChatContainerButtonAdd"
+                                    >
+                                        <SvgMicro />
+                                    </button>
+                                    <button
+                                        onClick={() => {}}
+                                        className="MainChatContainerButton"
+                                    >
+                                        <SvgArrow />
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
